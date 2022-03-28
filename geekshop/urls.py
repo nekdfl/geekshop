62b888d5807d88ce30f3dbb21b4f630ec9131b00
Николай Гаркушенко <nekdfl@gmail.com>
2022-03-28 12:25:39 +0300

    create urls.py for products change base.html (navbar  product_url) add fixtures change products.html  to use "for in" for peoducts list

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -13,14 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from mainapp.views import index
-from mainapp.views import products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', products, name='product'),
+    path('products/', include('mainapp.urls', namespace='mainapp_ns'))
+
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,32 @@
+import json
+from unicodedata import category
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request, 'mainapp/index.html')
 
-def products(request):
-    return render(request, 'mainapp/products.html')
+
+def index(request):
+    content = {
+        "title": "GeekShop"
+    }
+    return render(request, 'mainapp/index.html', content)
+
+
+def products_view(request):
+
+    categories = []
+    products = []
+
+    with open("fixtures/categories.json") as f:
+        categories = json.load(f)
+
+    with open("fixtures/products.json") as f:
+        products = json.load(f)
+
+    content = {
+        "title": "GeekShop - Каталог",
+        "categories": categories["category_list"],
+        "products": products["goods"]
+
+    }
+    return render(request, 'mainapp/products.html', content)
