a3cd4ac0c276ac97ba9030e2d7fcd495dc614a05
Николай Гаркушенко <nekdfl@gmail.com>
2022-03-30 13:28:39 +0300

    add support of media to project

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from mainapp.views import index
 
@@ -25,3 +27,5 @@ urlpatterns = [
     path('products/', include('mainapp.urls', namespace='mainapp_ns'))
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
