9429907195789136dbb78942efd5dcba3e91c2ee
Гаркушенко Николай <nekdfl@gmail.com>
2022-04-04 23:35:23 +0300

    fix name in model description field

--- mainapp/models.py
+++ mainapp/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
     '''model product'''
     name = models.CharField(max_length=128)
     image = models.ImageField(upload_to='product_images', blank=True)
-    desciption = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantily = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
