8bd5de2b7c2758003506502b09b9fcfe764f3ef8
Гаркушенко Николай <nekdfl@gmail.com>
2022-04-04 13:55:39 +0300

    authapp

--- authapp/models.py
+++ authapp/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
 class User(AbstractUser):
-    image = models.ImageField(upload_to="users_avatar", blank=True)
+    image = models.ImageField(upload_to="user_image", blank=True)
     age = models.PositiveIntegerField(default=18)
+
--- mainapp/management/commands/filldb.py
+++ mainapp/management/commands/filldb.py
@@ -7,29 +7,35 @@ from mainapp.models import ProductCategory, Product
 
 def load_from_json(file_name):
     with open(file_name, mode='r', encoding='utf-8') as infile:
-
         return json.load(infile)
+
+
+def load_category_fixture():
+    categories = load_from_json('mainapp/fixtures/dump_category.json')
+
+    ProductCategory.objects.all().delete()
+    for category in categories:
+        cat = category.get('fields')
+        cat['id'] = category.get('pk')
+        new_category = ProductCategory(**cat)
+        new_category.save()
+
+def load_product_fixture():
+    products = load_from_json('mainapp/fixtures/dump_product.json')
+    Product.objects.all().delete()
+    for product in products:
+        prod = product.get('fields')
+        category = prod.get('category')
+        _category = ProductCategory.objects.get(id=category)
+        prod['category'] =_category
+        new_category = Product(**prod)
+        new_category.save()
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        User.objects.create_superuser(username='nikolay',email='admin@mail.ru',password='1')
-        categories = load_from_json('mainapp/fixtures/category.json')
+        # User.objects.create_superuser(username='gns', email='admin@mail.ru', password='1')
+        load_category_fixture()
+        load_product_fixture()
 
-        ProductCategory.objects.all().delete()
-        for category in categories:
-            cat = category.get('fields')
-            cat['id'] = category.get('pk')
-            new_category = ProductCategory(**cat)
-            new_category.save()
-
-        products = load_from_json('mainapp/fixtures/products.json')
-        Product.objects.all().delete()
-        for product in products:
-            prod = product.get('fields')
-            category = prod.get('category')
-            _category = ProductCategory.objects.get(id=category)
-            prod['category'] =_category
-            new_category = Product(**prod)
-            new_category.save()
 
