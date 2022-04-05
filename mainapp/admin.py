3e13508b7389cc448373463b61c135d8a2e18808
Гаркушенко Николай <nekdfl@gmail.com>
2022-04-05 11:30:41 +0300

    fix user model already registred

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,10 +1,7 @@
-from itertools import product
 from django.contrib import admin
 
-from mainapp.models import Product, ProductCategory
-from authapp.models import User
-
 # Register your models here.
-admin.site.register(Product)
+from mainapp.models import Product, ProductCategory
+
 admin.site.register(ProductCategory)
-admin.site.register(User)
+admin.site.register(Product)
