0810e02cdaec1f5e26f0388dc493c63514f8d8fc
Николай Гаркушенко <nekdfl@gmail.com>
2022-04-05 17:41:12 +0300

    add username validator to model

--- authapp/models.py
+++ authapp/models.py
@@ -2,7 +2,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+from authapp.validators import validate_username
+
 
 class User(AbstractUser):
-    image = models.ImageField(upload_to="user_avatar",blank=True)
+    image = models.ImageField(upload_to="user_avatar", blank=True, validators=[validate_username])
     age = models.PositiveIntegerField(default=18)
