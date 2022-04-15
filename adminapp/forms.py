51f49b387f1fc180b1220e522e55f6027eed1c07
Николай Гаркушенко <nekdfl@gmail.com>
2022-04-15 17:38:05 +0300

    CRUD for ProductCategory and Product

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -1,6 +1,8 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from authapp.models import User
+from mainapp.models import ProductCategory, Product
 
 
 class UserAdminRegisterForm(UserCreationForm):
@@ -34,8 +36,77 @@ class AdminUserProfileForm(UserChangeForm):
         super(UserChangeForm, self).__init__(*args, **kwargs)
         self.fields['username'].widget.attrs['readonly'] = True
 
+        for filed_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+
+        self.fields['image'].widget.attrs['class'] = 'custom-file-input'
+
+
+############################
+class AdminCategoryAdd(forms.ModelForm):
+    class Meta:
+        model = ProductCategory
+        fields = ('name', 'description')
+
+    def __init__(self, *args, **kwargs):
+        super(AdminCategoryAdd, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs['placeholder'] = 'Введите имя категории'
+        self.fields['description'].widget.attrs['placeholder'] = 'Введите описание...'
 
         for filed_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control py-4'
+
+
+class AdminCategoryUpdate(forms.ModelForm):
+    class Meta:
+        model = ProductCategory
+        fields = ('name', 'description')
+
+    def __init__(self, *args, **kwargs):
+        super(AdminCategoryUpdate, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs['placeholder'] = 'Введите имя категории'
+        self.fields['description'].widget.attrs['placeholder'] = 'Введите описание...'
+
+        for filed_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+
+
+############################
+class AdminProductAdd(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ('name', 'image', 'description', 'price', 'quantity', 'category')
+
+    def __init__(self, *args, **kwargs):
+        super(AdminProductAdd, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs['placeholder'] = 'Введите наименование товара'
+        self.fields['image'].widget.attrs['placeholder'] = 'Загрузить фото'
+        self.fields['description'].widget.attrs['placeholder'] = 'Введите описание'
+        self.fields['price'].widget.attrs['placeholder'] = 'Укажите цену'
+        self.fields['quantity'].widget.attrs['placeholder'] = 'Укажите колличество на складе'
+        self.fields['category'].widget.attrs['placeholder'] = 'Выбирите категорию'
+
+        for filed_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-2'
+
+        self.fields['image'].widget.attrs['class'] = 'custom-file-input'
+
+
+class AdminProductUpdate(AdminProductAdd):
+    class Meta:
+        model = Product
+        fields = ('name', 'image', 'description', 'price', 'quantity', 'category')
+
+    def __init__(self, *args, **kwargs):
+        super(AdminProductUpdate, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs['placeholder'] = 'Введите наименование товара'
+        self.fields['image'].widget.attrs['placeholder'] = 'Загрузить фото'
+        self.fields['description'].widget.attrs['placeholder'] = 'Введите описание'
+        self.fields['price'].widget.attrs['placeholder'] = 'Укажите цену'
+        self.fields['quantity'].widget.attrs['placeholder'] = 'Укажите колличество на складе'
+        self.fields['category'].widget.attrs['placeholder'] = 'Выбирите категорию'
+
+        for filed_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-2'
 
         self.fields['image'].widget.attrs['class'] = 'custom-file-input'
--- adminapp/views.py
+++ adminapp/views.py
@@ -5,8 +5,10 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from adminapp.forms import UserAdminRegisterForm, AdminUserProfileForm
+from adminapp.forms import UserAdminRegisterForm, AdminUserProfileForm, AdminCategoryAdd, AdminCategoryUpdate, \
+    AdminProductAdd, AdminProductUpdate
 from authapp.models import User
+from mainapp.models import ProductCategory, Product
 
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -16,7 +18,6 @@ def index(request):
     }
     return render(request, 'adminapp/admin.html', context)
 
-
 @user_passes_test(lambda u: u.is_superuser)
 def admin_users(request):
     context = {
@@ -24,7 +25,6 @@ def admin_users(request):
         'users': User.objects.all()
     }
     return render(request, 'adminapp/admin-users-read.html', context)
-
 
 @user_passes_test(lambda u: u.is_superuser)
 def admin_user_create(request):
@@ -44,7 +44,6 @@ def admin_user_create(request):
         'form': form
     }
     return render(request, 'adminapp/admin-users-create.html', context)
-
 
 @user_passes_test(lambda u: u.is_superuser)
 def admin_user_update(request, id):
@@ -68,8 +67,120 @@ def admin_user_update(request, id):
 
 @user_passes_test(lambda u: u.is_superuser)
 def admin_user_delete(request, id):
-    # user_select = User.objects.get(id=id).delete()
-    user_select = User.objects.get(id=id)
-    user_select.is_active = False
-    user_select.save()
+    user_select = User.objects.get(id=id).delete()
+    # user_select = User.objects.get(id=id)
+    # user_select.is_active = False
+    # user_select.save()
     return HttpResponseRedirect(reverse('adminapp:admin_users'))
+
+
+#########################
+@user_passes_test(lambda u: u.is_superuser)
+def admin_categories(request):
+    context = {
+        'title': 'Админка | Пользователи',
+        'categories': ProductCategory.objects.all()
+    }
+    return render(request, 'adminapp/admin-categories-read.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_category_add(request):
+    if request.method == 'POST':
+        form = AdminCategoryAdd(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Категория успешно добавлена')
+            return HttpResponseRedirect(reverse('adminapp:admin_categories'))
+        else:
+            messages.error(request, form.errors)
+            print(form.errors)
+    else:
+        form = AdminCategoryAdd
+    context = {
+        'title': 'Админка | Добавить категорию',
+        'form': form
+    }
+    return render(request, 'adminapp/admin-categories-create.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_category_update(request, id):
+    category_select = ProductCategory.objects.get(id=id)
+    if request.method == "POST":
+        form = AdminCategoryUpdate(data=request.POST, instance=category_select)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('adminapp:admin_categories'))
+    else:
+        form = AdminCategoryUpdate(instance=category_select)
+
+    context = {
+        'title': 'Админка | Обновление категории',
+        'form': form,
+        'category_select': category_select
+    }
+    return render(request, 'adminapp/admin-categories-update-delete.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_category_delete(request, id):
+    ProductCategory.objects.get(id=id).delete()
+    return HttpResponseRedirect(reverse('adminapp:admin_categories'))
+
+
+#########################
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_products(request):
+    context = {
+        'title': 'Админка | Товары',
+        'products': Product.objects.all()
+    }
+    return render(request, 'adminapp/admin-products-read.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_product_add(request):
+    if request.method == 'POST':
+        form = AdminProductAdd(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Товар успешно добавлен')
+            return HttpResponseRedirect(reverse('adminapp:admin_products'))
+        else:
+            messages.error(request, form.errors)
+            print(form.errors)
+    else:
+        form = AdminProductAdd()
+    context = {
+        'title': 'Админка | Создание товара',
+        'form': form
+    }
+    return render(request, 'adminapp/admin-products-create.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_product_update(request, id):
+    product_select = Product.objects.get(id=id)
+    if request.method == "POST":
+        form = AdminProductUpdate(data=request.POST, instance=product_select, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('adminapp:admin_users'))
+    else:
+        form = AdminProductUpdate(instance=product_select)
+
+    context = {
+        'title': 'Админка | Обновление информации о товаре',
+        'form': form,
+        'product_select': product_select
+    }
+    return render(request, 'adminapp/admin-products-update-delete.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_product_delete(request, id):
+    Product.objects.get(id=id).delete()
+
+    return HttpResponseRedirect(reverse('adminapp:admin_products'))
