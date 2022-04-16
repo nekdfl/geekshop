2c8b77f19e6e8c91fabbe4e55282ca004a4c920e
Николай Гаркушенко <nekdfl@gmail.com>
2022-04-16 20:24:08 +0300

    rewrite admin user to CBV

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,19 +1,19 @@
 from django.urls import path
 
+from adminapp.views import IndexTemplateView, UserListView, UserCreateView, UserUpdateView, UserDeleteView
 from adminapp.views import admin_categories, admin_category_add, admin_category_update, admin_category_delete
 from adminapp.views import admin_products, admin_product_add, admin_product_update, admin_product_delete
-from adminapp.views import index, admin_users, admin_user_create, admin_user_delete, admin_user_update
 
 app_name = "adminapp"
 urlpatterns = [
-    path('', index, name="index"),
-    path('users/', admin_users, name='admin_users'),
-    path('user-create/', admin_user_create, name='admin_user_create'),
-    path('user-update/<int:id>/', admin_user_update, name='admin_user_update'),
-    path('user-delete/<int:id>/', admin_user_delete, name='admin_user_delete'),
+    path('', IndexTemplateView.as_view(), name="index"),
+    path('users/', UserListView.as_view(), name='admin_users'),
+    path('user-create/', UserCreateView.as_view(), name='admin_user_create'),
+    path('user-update/<int:pk>/', UserUpdateView.as_view(), name='admin_user_update'),
+    path('user-delete/<int:pk>/', UserDeleteView.as_view(), name='admin_user_delete'),
     path('categories/', admin_categories, name='admin_categories'),
     path('category-add/', admin_category_add, name='admin_category_add'),
-    path('category-update/<int:id>/', admin_category_update, name='admin_category_update'),
+    path('category-update/<int:pk>/', admin_category_update, name='admin_category_update'),
     path('category-delete/<int:id>/', admin_category_delete, name='admin_category_delete'),
     path('products/', admin_products, name='admin_products'),
     path('product-add/', admin_product_add, name='admin_product_add'),
--- adminapp/views.py
+++ adminapp/views.py
@@ -3,75 +3,130 @@ from django.contrib import messages
 from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView, UpdateView, DeleteView, TemplateView, CreateView
 
 from adminapp.forms import UserAdminRegisterForm, AdminUserProfileForm, AdminCategoryAdd, AdminCategoryUpdate, \
     AdminProductAdd, AdminProductUpdate
+from adminapp.mixins import BaseClassContextMixin, SuperUserDispatchMixin, UserDispatchMixin
 from authapp.models import User
 from mainapp.models import ProductCategory, Product
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def index(request):
-    context = {
-        'title': 'Админка | Пользователи'
-    }
-    return render(request, 'adminapp/admin.html', context)
+# пример function based view с декоратором
+# @user_passes_test(lambda u: u.is_superuser)
+# def index(request):
+#     context = {
+#         'title': 'Админка | Пользователи'
+#     }
+#     return render(request, 'adminapp/admin.html', context)
 
-@user_passes_test(lambda u: u.is_superuser)
-def admin_users(request):
-    context = {
-        'title': 'Админка | Пользователи',
-        'users': User.objects.all()
-    }
-    return render(request, 'adminapp/admin-users-read.html', context)
+# прмиер class based view и работа с конектостом через переделение встроенных методгов
+class IndexTemplateView(TemplateView):
+    template_name = 'adminapp/admin.html'
 
-@user_passes_test(lambda u: u.is_superuser)
-def admin_user_create(request):
-    if request.method == 'POST':
-        form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Регистрация успешно завершена')
-            return HttpResponseRedirect(reverse('adminapp:admin_users'))
-        else:
-            messages.error(request, form.errors)
-            print(form.errors)
-    else:
-        form = UserAdminRegisterForm
-    context = {
-        'title': 'Админка | Регистрация пользователи',
-        'form': form
-    }
-    return render(request, 'adminapp/admin-users-create.html', context)
+    # применение декоратора как FBV index выше
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(IndexTemplateView, self).dispatch(request, *args, **kwargs)
 
-@user_passes_test(lambda u: u.is_superuser)
-def admin_user_update(request, id):
-    pass
-    user_select = User.objects.get(id=id)
-    if request.method == "POST":
-        form = AdminUserProfileForm(data=request.POST, instance=user_select, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('adminapp:admin_users'))
-    else:
-        form = AdminUserProfileForm(instance=user_select)
-
-    context = {
-        'title': 'Админка | Обновление пользователи',
-        'form': form,
-        'user_select': user_select
-    }
-    return render(request, 'adminapp/admin-users-update-delete.html', context)
+    # работа с конекстом
+    def get_context_data(self, **kwargs):
+        context = super(IndexTemplateView, self).get_context_data(**kwargs)
+        context['title'] = 'Главная'
+        return context
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def admin_user_delete(request, id):
-    user_select = User.objects.get(id=id).delete()
-    # user_select = User.objects.get(id=id)
-    # user_select.is_active = False
-    # user_select.save()
-    return HttpResponseRedirect(reverse('adminapp:admin_users'))
+# @user_passes_test(lambda u: u.is_superuser)
+# def admin_users(request):
+#     context = {
+#         'title': 'Админка | Пользователи',
+#         'users': User.objects.all()
+#     }
+#     return render(request, 'adminapp/admin-users-read.html', context)
+
+# прмиер CBV с использованием миксинов
+class UserListView(ListView, BaseClassContextMixin, SuperUserDispatchMixin):
+    model = User
+    template_name = 'adminapp/admin-users-read.html'
+    title = 'Админка | Пользователи'
+    context_object_name = 'users'
+
+
+# @user_passes_test(lambda u: u.is_superuser)
+# def admin_user_create(request):
+#     if request.method == 'POST':
+#         form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, 'Регистрация успешно завершена')
+#             return HttpResponseRedirect(reverse('adminapp:admin_users'))
+#         else:
+#             messages.error(request, form.errors)
+#             print(form.errors)
+#     else:
+#         form = UserAdminRegisterForm
+#     context = {
+#         'title': 'Админка | Регистрация пользователи',
+#         'form': form
+#     }
+#     return render(request, 'adminapp/admin-users-create.html', context)
+
+class UserCreateView(CreateView, BaseClassContextMixin, SuperUserDispatchMixin, UserDispatchMixin):
+    model = User
+    title = 'Админка | Регистрация пользователи'
+    template_name = 'adminapp/admin-users-create.html'
+    form_class = UserAdminRegisterForm
+    success_url = reverse_lazy('adminapp:admin_users')
+
+    def form_valid(self, form):
+        messages.success(self.request, f'Пользователь {form.clean()["username"]} успешно зарегистрирован')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, form.errors)
+        return super().form_invalid(form)
+
+
+class UserUpdateView(UpdateView, BaseClassContextMixin, SuperUserDispatchMixin, UserDispatchMixin):
+    model = User
+    title = 'Админка | Редактирование пользователи'
+    template_name = 'adminapp/admin-users-update-delete.html'
+    form_class = AdminUserProfileForm
+    success_url = reverse_lazy('adminapp:admin_users')
+
+    def form_valid(self, form):
+        messages.success(self.request, f'Пользователь {form.clean()["username"]} успешно обновлен')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, form.errors)
+        return super().form_invalid(form)
+
+
+# Пример удаления через DeleteView
+class UserDeleteView(DeleteView, SuperUserDispatchMixin, UserDispatchMixin):
+    model = User
+    template_name = 'adminapp/admin-users-update-delete.html'
+    form_class = AdminUserProfileForm
+    success_url = reverse_lazy('adminapp:admin_users')
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.is_active = False
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+
+# Пример удаления через FBV
+# @user_passes_test(lambda u: u.is_superuser)
+# def admin_user_delete(request, id):
+#     user_select = User.objects.get(id=id).delete()
+#     # user_select = User.objects.get(id=id)
+#     # user_select.is_active = False
+#     # user_select.save()
+#     return HttpResponseRedirect(reverse('adminapp:admin_users'))
 
 
 #########################
@@ -105,8 +160,8 @@ def admin_category_add(request):
 
 
 @user_passes_test(lambda u: u.is_superuser)
-def admin_category_update(request, id):
-    category_select = ProductCategory.objects.get(id=id)
+def admin_category_update(request, pk):
+    category_select = ProductCategory.objects.get(id=pk)
     if request.method == "POST":
         form = AdminCategoryUpdate(data=request.POST, instance=category_select)
         if form.is_valid():
@@ -124,8 +179,8 @@ def admin_category_update(request, id):
 
 
 @user_passes_test(lambda u: u.is_superuser)
-def admin_category_delete(request, id):
-    ProductCategory.objects.get(id=id).delete()
+def admin_category_delete(request, pk):
+    ProductCategory.objects.get(id=pk).delete()
     return HttpResponseRedirect(reverse('adminapp:admin_categories'))
 
 
