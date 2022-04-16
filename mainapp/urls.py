ecf76528324e92df641d3a885128b5ba271d9b21
Николай Гаркушенко <nekdfl@gmail.com>
2022-04-16 16:01:53 +0300

    Product Detail Paginator all categories at product page

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from mainapp.views import products
+from mainapp.views import products, ProductDetail
 
 app_name = "mainapp"
 
 urlpatterns = [
     path('', products, name='product'),
     path('category/<int:id_category>/', products, name='category'),
-    path('category/<int:id_category>/', products, name='category')
+    path('page/<int:page>/', products, name='category_page'),
+    path('detail/<int:pk>/', ProductDetail.as_view(), name='product_detail')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,6 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
+from django.views.generic import DetailView
 
 from mainapp.models import ProductCategory, Product
 
@@ -19,10 +21,24 @@ def products(request, id_category=None, page=1):
     else:
         products = Product.objects.all()
 
+    pagination = Paginator(products, per_page=2)
+
+    try:
+        products_pagination = pagination.page(page)
+    except PageNotAnInteger:
+        products_pagination = pagination.page(1)
+    except EmptyPage:
+        products_pagination = pagination.page(pagination.num_pages)
+
     content = {
         "title": "GeekShop - Каталог",
         "categories": ProductCategory.objects.all(),
-        "products": products
-
+        "products": products_pagination,
+        'current_page': page
     }
     return render(request, 'mainapp/products.html', content)
+
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = "mainapp/detail.html"
