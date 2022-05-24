124ea3c7ea88210bb1587b59ccb625e3fff6fa9d
Николай Гаркушенко <nekdfl@gmail.com>
2022-05-24 16:45:09 +0300

    static path update for production

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -187,7 +187,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (BASE_DIR / 'static',)
+if PRODUCTION:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+else:
+    STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
